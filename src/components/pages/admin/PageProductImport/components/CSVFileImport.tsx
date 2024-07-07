75f74163a2197664ba8683bee9d6d8e638d8b5aa
furiousme <shuptaroks@gmail.com>
2024-07-07 17:46:33 +0300

    feat(import-file): provide credentials to get signed url endpoint

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -35,13 +35,17 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       params: {
         name: encodeURIComponent(file.name),
       },
+      headers: {
+        Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+      },
     });
     console.log("File to upload: ", file.name);
     console.log("Uploading to: ", response.data.url);
+
     const result = await fetch(response.data.url, {
       method: "PUT",
       body: file,
-    }); 
+    });
     console.log("Result: ", result);
     setFile(undefined);
   };
