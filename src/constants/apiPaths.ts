53c4c7cb7a1e740df8d3d48049d9afd1369439b7
furiousme <shuptaroks@gmail.com>
2024-08-05 23:06:14 +0300

    feat(cart): update cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://scavie2it6.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://kkd22giog0.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://furiousme-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
